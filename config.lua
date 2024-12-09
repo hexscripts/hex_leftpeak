6ce462a482aea467aca0c3096f0ea505d59ff170
xLeon0666 <48410161+xLeon0666@users.noreply.github.com>
2024-12-09 03:37:26 +0100

    push

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {
+    easeTime = 100, -- Transition time in milliseconds
+    leftRange = 1.0, -- from 0.5 to 1.20
+    keymapping = {
+        name = "hex_leftpeak",
+        description = "Toggle Left Shoulder",
+        key = "H"
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+author 'xLeon0666 - discord.gg/hexscripts'
+description 'Hex Leftpeak'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
