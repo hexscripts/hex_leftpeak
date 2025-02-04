e3ef0bbe91837205ace7ef6a7afdf497f2d40023
yiruzu <yiruzu@protonmail.com>
2025-02-04 17:53:42 +0100

    refactor(config): slightly increase transition time for smoother transition

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {
-	transitionTime = 100, -- Time for the camera transition in milliseconds
+	transitionTime = 150, -- Time for the camera transition in milliseconds
 	leftOffset = 0.8, -- Camera left offset value | Range: 0.5 to 1.20
 	keymapping = {
 		name = "hex_leftpeak", -- Name of the key mapping
